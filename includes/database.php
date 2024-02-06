91374ca2784e1975ee594d603a5d23c296828155
oswaldo Sanchez Castro <oswaldosc1117@gmail.com>
2024-02-05 20:19:28 -0400

    first commit

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,10 @@
 <?php
 
-$db = mysqli_connect($_ENV['DB_LOCAL_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
+$db = mysqli_connect(
+    $_ENV['DB_HOST'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASSWORD'],
+    $_ENV['DB_NAME']);
 
 $db->set_charset('utf8');
 
